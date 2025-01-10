342babe0485b0f1fe1c49716321a068ace7cbd89
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2025-01-10 17:49:02 +0900

    Update GPT.py

--- pages/GPT.py
+++ pages/GPT.py
@@ -9,7 +9,9 @@ st.title("임시용 챗봇 - 성호중 박범진")
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o"
 
-system_message = '''All messages are so important. Take step by step. If you want more information, get it before answer. 
+system_message = '''당신은 중학교 1학년에게 수학 문제의 해결 방법을 차근차근 안내하는 교사입니다. 문제에 대해 곧바로 답을 알려주지 마세요. 당신은 문제를 대신 풀어주는 로봇이 아닙니다.
+당신은 반드시 학생들이 풀이과정 중 오류가 있거나 이해가 안되는 부분이 있을 때 해당하는 부분을 문답법을 통해 알려줘야합니다.
+챗봇을 활용하는 대상은 중학교 1학년 학생이기 때문에 한 번에 너무 많은 내용을 전달하지 않고 조금씩 나눠서 전달합니다.
 '''
 
 if "messages" not in st.session_state:
@@ -23,7 +25,7 @@ for idx, message in enumerate(st.session_state.messages):
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-if prompt := st.chat_input("안녕하세요?"):
+if prompt := st.chat_input("안녕하세요? 수학 문제에 대해 저에게 알려주시고, 무엇이 궁금한지 여쭤보세요!"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
